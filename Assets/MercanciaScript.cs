cd50b88a9f3ea0f305c8ef79aaf20496c830a339
Leonel Abud <abudleito@gmail.com>
2025-06-04 15:41:07 -0300

    Plata manager

--- Assets/MercanciaScript.cs
+++ Assets/MercanciaScript.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class MercanciaScript : MonoBehaviour
 {
     public int precio;
+    public MoneyManager moneyManager;
 
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log(collision.gameObject.name);
         if (collision.gameObject.name == "Player")
         {
-            Destroy(gameObject);
+            bool alcanza = moneyManager.UpdateMoney(-precio);
+            if (alcanza)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
---
+++ Assets/MoneyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public TextMeshProUGUI TxtMoney;
+
+    public bool UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            Debug.Log("No te alcanza");
+            return false;
+        }
+        else
+        {
+            money += amount;
+            TxtMoney.text = "$"+money.ToString();
+            return true;
+        }
+    }
+
+    void Start()
+    {
+        UpdateMoney(0);
+    }
+}
